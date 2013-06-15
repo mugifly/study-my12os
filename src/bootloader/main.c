fd3014720d1978fbc9477e13ec8b80548f073f8d
Masanori Ohgita <mp@ohgita.info>
2013-06-16 02:52:04 +0900

    Add(main): putxval sample

--- src/bootloader/main.c
+++ src/bootloader/main.c
@@ -5,8 +5,13 @@
 int main(void) {
 	/* Initialize the serial device */
 	serial_init(SERIAL_DEFAULT_DEVICE);
+
 	/* Output */
 	puts("Hello World!\n");
+	putxval(0x10, 0);
+	puts("\n");
+	putxval(0xffff, 0);
+	puts("\n");
 
 	while(1){
 
